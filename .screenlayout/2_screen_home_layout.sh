8da253a8d2545afafe8696859b425e96a56552b8
Nenad Pavlovic <nenad@batman.sentian.ai>
2020-03-14 23:22:43 +0100

    added screenlayouts

---
+++ .screenlayout/2_screen_home_layout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-2-1 --primary --mode 2560x1440 --pos 900x0 --rotate normal --output DP-2-2 --off --output DP-2-3 --mode 1440x900 --pos 0x0 --rotate right --output eDP-1 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --off
---
+++ .screenlayout/external_only.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-2-1 --off --output DP-2-2 --primary --mode 3440x1440 --pos 1920x0 --rotate normal --output DP-2-3 --off --output eDP-1 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --off
