bff3fed53d131e9ba41acc5847723f6e925c8cee
NePav <nenad.pavlovic@sentian.ai>
2020-12-06 16:24:17 +0100

    added script for copying and registering SSH keys

--- install.sh
+++ install.sh
@@ -12,6 +12,17 @@ sudo apt install git
 git config --global user.name   "NePav"
 git config --global user.email  "nenad.pavlovic@sentian.ai"
 
+## Copy and register old SSH keys
+#cp /path/to/my/key/id_rsa ~/.ssh/id_rsa
+#cp /path/to/my/key/id_rsa.pub ~/.ssh/id_rsa.pub
+## change permissions on file
+#sudo chmod 600 ~/.ssh/id_rsa
+#sudo chmod 600 ~/.ssh/id_rsa.pub
+## start the ssh-agent in the background
+#eval $(ssh-agent -s)
+## make ssh agent to actually use copied key
+#ssh-add ~/.ssh/id_rsa
+
 ## Git clone my config files 
 git clone https://github.com/NePav/dotFiles.git ~/Documents/MyGits
 
